cf25bfe0a1aa87882b161aa7ed22f73f3cf9b495
Akash Das <mfs.akash@gmail.com>
2023-04-30 21:10:53 +0530

    added test cases

--- typescript/projects/Solution - Car Rental System.ts
+++ typescript/projects/Solution - Car Rental System.ts
@@ -88,3 +88,13 @@ class CarRental implements Car {
     }
   }
 }
+
+const carRental = new CarRental(1, "Toyota", "Yaris", 2018);
+console.log(carRental);
+carRental.reserve(new CarUser(1, "John"), 3);
+carRental.reserve(new CarUser(2, "Jane"), 5);
+carRental.extendReservation(new CarUser(1, "John"), 2);
+carRental.extendReservation(new CarUser(2, "Jane"), 2);
+carRental.returnCar(new CarUser(1, "John"));
+carRental.returnCar(new CarUser(2, "Jane"));
+console.log(carRental);
